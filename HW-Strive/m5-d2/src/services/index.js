2886a3bf46e605d2d28ecde2242758684dee6f14
Hammad-87 <pakswim@gmail.com>
2020-06-23 16:44:45 +0200

    Get, Post, Put, Delete

--- HW-Strive/m5-d2/src/services/index.js
+++ HW-Strive/m5-d2/src/services/index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const fs = require("fs")
 const path = require("path")
-//const uniqid = require("uniqid")
+const uniqid = require("uniqid")
+const { request, response } = require("express")
 
 
 const router = express.Router()
@@ -12,27 +13,65 @@ const studentsFilePath = path.join(__dirname, "Students.json")
 //1
 router.get("/", (request, response) => {
     const fileContentAsBuffer = fs.readFileSync(studentsFilePath)
-    console.log(fileContentAsBuffer)
+    //console.log(fileContentAsBuffer)
     const fileContent = fileContentAsBuffer.toString()
     console.log(fileContent)
 
     response.send(JSON.parse(fileContent))
 })
+
+
 //2
-/* router.get("/Students/:id", (request, response) => {
+ router.get("/:id", (request, response) => {
     const fileContentAsBuffer = fs.readFileSync(studentsFilePath)
     const studentsArray = JSON.parse(fileContentAsBuffer.toString())
-    console.log(studentsArray)
+    //console.log(studentsArray)
     const student = studentsArray.filter(student => student.id ===request.params.id)
     console.log(student)
 
     response.send(student)
 
 })
- */
+ 
+//3
+router.post("/", (request, response) => {
+    console.log(request.body)
+    const newStudent = { ...request.body, id:uniqid()}
 
+    const fileContentAsBuffer = fs.readFileSync(studentsFilePath)
+    const studentsArray = JSON.parse(fileContentAsBuffer.toString())
 
+    studentsArray.push(newStudent)
+    fs.writeFileSync(studentsFilePath,JSON.stringify(studentsArray))
+    
+    response.status(201).send(newStudent)
+})
 
+//4
+router.put("/:id", (request, response) => {
+    const fileContentAsBuffer = fs.readFileSync(studentsFilePath)
+    const studentsArray = JSON.parse(fileContentAsBuffer.toString())
+
+    const filteredStudentsArray = studentsArray.filter((student) => student.id !== request.params.id)
+
+    const student = request.body 
+    student.id = request.params.id
+
+    filteredStudentsArray.push(student)
+
+    fs.writeFileSync(studentsFilePath, JSON.stringify(filteredStudentsArray))
+    response.send("OK")
+
+})
+router.delete("/id:", (request, response) => {
+    const fileContentAsBuffer = fs.readFileSync(studentsFilePath)
+    const studentsArray = JSON.parse(fileContentAsBuffer.toString())
+
+    const filteredStudentsArray = studentsArray.filter((student) => student.id !== request.params.id)
+fs.writeFileSync(studentsFilePath, JSON.stringify(filteredStudentsArray))
+
+response.send("OK")
+})
 
 
 
