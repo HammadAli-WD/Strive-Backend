ec179268f2f671958c4d4d0a121290b86ee2df37
Hammad-87 <pakswim@gmail.com>
2020-06-24 22:44:06 +0200

    updated

--- HW-Strive/m5-d3/src/server.js
+++ HW-Strive/m5-d3/src/server.js
@@ -1,9 +1,41 @@
-const express = require ("express")
+const express = require("express")
+const listEndpoints = require("express-list-endpoints")
+const projectsRouter = require("./services/projects")
+//const moviesRouter = require("./services/movies")
+//const problematicRoutes = require("./services/problematicRoutes")
+const {
+  notFoundHandler,
+  unauthorizedHandler,
+  forbiddenHandler,
+  catchAllHandler,
+} = require("./services/errorHandling")
 
 const server = express()
-const port = process.env.PORT || 3002
 
-server.use(express.json())
+const port = process.env.PORT 
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(`Logged ${req.url} ${req.method} -- ${new Date()}`)
+  next()
+}
+
+server.use(express.json()) // Built in middleware
+server.use(loggerMiddleware)
+
+// ROUTES
+server.use("/projects", projectsRouter)
+//server.use("/movies", moviesRouter)
+//server.use("/problems", problematicRoutes)
+
+// ERROR HANDLERS
+
+server.use(notFoundHandler)
+server.use(unauthorizedHandler)
+server.use(forbiddenHandler)
+server.use(catchAllHandler)
+
+console.log(listEndpoints(server))
+
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`)
 })
--- HW-Strive/m5-d3/src/services/students/index.js
+++ HW-Strive/m5-d3/src/services/students/index.js
@@ -1,28 +1,71 @@
-const express = require("express")
-const fs = require ("fs")
-const path = require ("path")
-const uniqid = require ("uniqid")
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
+const uniqid = require("uniqid");
+const router = express.Router();
 
-const {check, validationResult} = require("express-validator")
+const fileDirectory = path.join(__dirname, "students.json");
+router.get("/", (req, resp) => {
+  const studentsList = fs.readFileSync(fileDirectory);
+  resp.send(JSON.parse(studentsList.toString()));
+});
 
-const router = express.Router()
+router.get("/:id", (req, res) => {
+  const query = req.params.id;
+  const fileContent = fs.readFileSync(fileDirectory);
+  const arr = JSON.parse(fileContent.toString());
+  const user = arr.find((student) => student.id === query);
+  res.send(user);
+});
+router.post("/", (req, res) => {
+  const fileContent = fs.readFileSync(fileDirectory);
+  const arr = JSON.parse(fileContent.toString());
+  const user = req.body;
+  if (!_.has(user, "name") || user.name === "") {
+    res.status(400).send("name is required");
+  } else if (!_.has(user, "surname") || user.surname === "") {
+    res.status(400).send("surname is required");
+  } else if (!_.has(user, "email") || user.email === "") {
+    res.status(400).send("email is required");
+  } else if (!_.has(user, "birthDate") || user.birthDate === "") {
+    res.status(400).send("birthDate is required");
+  } else if (arr.filter((student) => student.email === user.email).length > 0) {
+    res.status(400).send("Email must be unique");
+  } else {
+    const newUser = { ...user, id: uniqid() };
+    arr.push(newUser);
+    fs.writeFileSync(fileDirectory, JSON.stringify(arr));
+    res.status(201).send();
+  }
+});
+router.put("/:id", (req, res) => {
+  const query = req.params.id;
+  const fileContent = fs.readFileSync(fileDirectory);
+  const studentsList = JSON.parse(fileContent.toString());
+  if (
+    studentsList.filter(
+      (student) => student.email === req.body.email && student.id !== query
+    ).length > 0
+  ) {
+    res.status(400).send("Email must be unique");
+  } else {
+    const updatedStudentsList = studentsList.map(
+      (student) =>
+        (student.id === query && { ...req.body, id: query }) || student
+    );
 
-const readFile = (fileName) => {
-const fileContent = fs.readFileSync(path.join(__dirname, fileName))
-return JSON.parse(fileContent.toString())
-}
+    fs.writeFileSync(fileDirectory, JSON.stringify(updatedStudentsList));
+    res.send(updatedStudentsList);
+  }
+});
+router.delete("/:id", (req, res) => {
+  const query = req.params.id;
+  const fileContent = fs.readFileSync(fileDirectory);
+  const studentsList = JSON.parse(fileContent.toString());
+  const filtered = studentsList.filter((student) => student.id !== query);
+  fs.writeFileSync(fileDirectory, JSON.stringify(filtered));
+  res.send(filtered);
+});
 
-router.get("/:id", (req, res, next) =>{
-    try {
-        const studentsDB = readFile("students.json")
-        const student = studentsDB.filter((student) => student.ID === req.params.id)
-        res.send(student)
-        
-    } catch (error) {
-        error.httpStatusCode = 404
-        next(error)
-    }
-})
-
-
-module.exports = router
+module.exports = router;
